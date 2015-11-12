28b599e7879252bc8152099a84e66abd8dc97063
Robert Onodi <robert.onodi89@gmail.com>
2015-11-13 01:00:04 +0200

    auth service base project

---
+++ auth-service/tests/integration.test.js
@@ -0,0 +1,96 @@
+'use strict';
+
+/**
+ * Important! Set the environment to test
+ */
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var should = chai.should();
+var app = require('../index');
+var config = require('../config/environments/test');
+
+chai.use(require('chai-things'));
+
+describe('entity:auth', function() {
+  var _user = {};
+
+  before(function(done) {
+
+    // mock user service
+    app.define({
+      entity: 'user',
+      action: 'verify'
+    }, function(args, callback) {
+      // generate random id
+      args.id = Date.now;
+
+      callback(null, args);
+    });
+
+    app.listen({ port: config.port, hostname: config.hostname }, function(err, addr) {
+      if (err) throw err;
+
+      done();
+    });
+  });
+
+  after(function(done) {
+    app.knex('tokens').truncate().asCallback(function(err) {
+      if (err) throw err;
+
+      app.close(function() {
+        done();
+      });
+    })
+  });
+
+  describe('action:verify_user', function() {
+    it('should authenticate user and generate token if user has valid credentials', function(done) {
+      app.exec({
+        entity: 'auth',
+        action: 'verify_user',
+        data: {
+          email: 'john.doe@test.com',
+          password: 'pass1234',
+        }
+      }, function(err, user) {
+        if (err) throw err;
+
+        should.exist(user.id);
+        should.exist(user.token);
+        should.not.exist(user.password);
+        should.not.exist(user.salt);
+        user.name.should.equal('John Doe');
+        user.email.should.equal('john.doe@test.com');
+
+        _user = user;
+
+        done();
+      });
+    });
+  });
+
+  describe('action:verify_token', function() {
+    it('should verify if a token is valid', function(done) {
+      app.exec({
+        entity: 'auth',
+        action: 'verify_token',
+        data: {
+          token: _user.token
+        }
+      }, function(err, user) {
+        if (err) throw err;
+
+        should.not.exist(user.password);
+        should.not.exist(user.salt);
+        user.id.should.equal(_user.id);
+        user.name.should.equal(_user.name);
+        user.email.should.equal(_user.email);
+
+        done();
+      });
+    });
+  });
+
+});
